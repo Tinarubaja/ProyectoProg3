49cda6b5cd0cdd84c633b55ba897eb22ddf46da7
Tati Lamas <tatilamas@MacBook-Pro-de-Tati.local>
2023-09-08 16:48:13 -0300

    Armando detalle artista

--- src/components/Canciones/artist.js
+++ src/components/Canciones/artist.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
+// import CardArtista from "../CardArtista"
+
 // class Artist extends Component{
-//     constructor(){
-//         super();
-//         this.state = {
-            
+//     constructor(props){
+//         super(props)
+//         this.state={
+//            id: Number(props.match.params.id),
+//            canciones: []
 //         }
+//         console.log(this.state.id)
 //     }
+
+//     componentDidMount(){
+//         let canciones1 = []
+//         let url = `` // Ver bien cual es la api 
+//         fetch(url)
+//             .then(response => response.json())
+//             .then(data => canciones1.push(data))
+//             .then(() => this.setState(
+//                 {
+//                     canciones:canciones1
+//                  }
+                    
+//             ))
+//             .catch(error => console.log('El error es' + error))
+//             console.log(this.state.canciones)
+//     }
+    
+
+//     render(){
+//         return(
+//             <React.Fragment >
+//            {this.state.canciones.map((detalleC, idx) => <CardArtista key={detalleC + idx} datosDetalle={detalleC} />)}
+//            </React.Fragment>
+
+//         )
+//     }
+
+// }
+
 
 function Artist(){
     return(
