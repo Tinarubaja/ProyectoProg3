53c640bf244f4fd51a8b52e6b38c24cb9b4c6de3
Tati Lamas <tatilamas@MBP-de-Tati.fibertel.com.ar>
2023-09-09 14:03:49 -0300

    detalle artista

--- src/components/Canciones/artist.js
+++ src/components/Canciones/artist.js
@@ -1,52 +1,52 @@
 import React, {Component} from 'react';
-// import CardArtista from "../CardArtista"
+import CardArtista from "../CardArtista"
 
-// class Artist extends Component{
-//     constructor(props){
-//         super(props)
-//         this.state={
-//            id: Number(props.match.params.id),
-//            canciones: []
-//         }
-//         console.log(this.state.id)
-//     }
+class Artist extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+           id: Number(props.match.params.id),
+           canciones: []
+        }
+        console.log(this.state.id)
+    }
 
-//     componentDidMount(){
-//         let canciones1 = []
-//         let url = `` // Ver bien cual es la api 
-//         fetch(url)
-//             .then(response => response.json())
-//             .then(data => canciones1.push(data))
-//             .then(() => this.setState(
-//                 {
-//                     canciones:canciones1
-//                  }
+    componentDidMount(){
+        let canciones1 = []
+        let url = `https://api.deezer.com/artist/${this.state.id}`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => canciones1.push(data))
+            .then(() => this.setState(
+                {
+                    canciones:canciones1
+                 }
                     
-//             ))
-//             .catch(error => console.log('El error es' + error))
-//             console.log(this.state.canciones)
-//     }
+            ))
+            .catch(error => console.log('El error es' + error))
+            console.log(this.state.canciones)
+    }
     
 
-//     render(){
-//         return(
-//             <React.Fragment >
-//            {this.state.canciones.map((detalleC, idx) => <CardArtista key={detalleC + idx} datosDetalle={detalleC} />)}
-//            </React.Fragment>
+    render(){
+        return(
+            <React.Fragment >
+           {this.state.canciones.map((detalleC, idx) => <CardArtista key={detalleC + idx} datosDetalle={detalleC} />)}
+           </React.Fragment>
 
-//         )
-//     }
+        )
+    }
 
-// }
-
-
-function Artist(){
-    return(
-        <div>
-            <h2>hola soy el componente</h2>
-        </div>
-    )
 }
+
+
+// function Artist(){
+//     return(
+//         <div>
+//             <h2>hola soy el componente</h2>
+//         </div>
+//     )
+// }
 
 
 
